37b3687ad3becb1ccbaa76e01f5222c91b464957
TKK <tk47@students.poly.edu>
2013-02-06 21:46:11 -0500

    Add evpy to setup.py; add entries to .gitignore.

--- src/setup.py
+++ src/setup.py
@@ -2,16 +2,25 @@
 
 from distutils.core import setup
 
-setup(name='TUF',
-      version='0.0',
-      description='A secure updater framework for Python',
-      author='lots of people',
-      url='https://updateframework.com',
-      packages=['tuf', 
-		'tuf.repo', 
-		'tuf.client', 
-		'tuf.pushtools', 
-		'tuf.pushtools.transfer',
-		'simplejson'],
-      scripts=['quickstart.py', 'tuf/pushtools/push.py', 'tuf/pushtools/receivetools/receive.py', 'tuf/repo/signercli.py']
-     )
+setup(
+    name        = 'TUF',
+    version     = '0.1',
+    description = 'A secure updater framework for Python',
+    author      = 'lots of people',
+    url         = 'https://updateframework.com',
+    packages    = [
+        'evpy',
+        'tuf',
+        'tuf.repo',
+        'tuf.client',
+        'tuf.pushtools',
+        'tuf.pushtools.transfer',
+        'simplejson'
+    ],
+    scripts=[
+        'quickstart.py',
+        'tuf/pushtools/push.py',
+        'tuf/pushtools/receivetools/receive.py',
+        'tuf/repo/signercli.py'
+    ]
+)
