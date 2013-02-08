e1b70000641dff6650594756e65c3ab937ded8ae
dachshund <trishankkarthik@gmail.com>
2013-02-08 11:14:45 -0500

    Better module organization.

--- src/tuf/interposition.py
+++ src/tuf/interposition.py
@@ -2,13 +2,8 @@ import os.path
 import tempfile
 import tuf.client.updater
 import tuf.conf
-import types
 import urllib
 import urlparse
-
-
-# hostname: str -> tuf_configuration: TUFConfiguration
-_tuf_configurations = {}
 
 
 class TUFConfiguration( object ):
@@ -18,68 +13,41 @@ class TUFConfiguration( object ):
         self.repository_mirrors = repository_mirrors
         self.tempdir = tempfile.mkdtemp()
 
-        # must switch context before instantiating updater
-        # because updater depends on some module (tuf.conf) variables
-        self.switch_context()
-        self.updater = tuf.client.updater.Updater(
-            hostname,
-            repository_mirrors
-        )
-
-    def make_tempfile( self, target_filepath ):
-        return self.tempdir, os.path.join( self.tempdir, target_filepath )
-
-    def switch_context( self ):
-        # Set the local repository directory containing the metadata files.
-        tuf.conf.repository_directory = self.repository_directory
-
 
 class TUFancyURLOpener( urllib.FancyURLopener ):
     # TODO: replicate complete behaviour of urllib.URLopener.open
-    def __tuf_open( self, parsed_url, tuf_configuration, data = None ):
-        def getcode( self ):
-            return 200
+    def __tuf_open( self, tuf_updater, data = None ):
+        filename, headers = self.__tuf_retrieve( tuf_updater, data = data )
 
-        def geturl( self ):
-            return fullurl
-
-        def info( self ):
-            raise NotImplementedError
-
-        # get a file-like object for free
-        filename, headers = self.__tuf_retrieve(
-            parsed_url,
-            tuf_configuration,
-            data = data
-        )
-
-        tempfile = TUFile( filename )
+        # TODO: like tempfile, ensure file is deleted when closed?
+        tempfile = open( filename )
         # extend tempfile with info(), getcode(), geturl()
         # http://docs.python.org/2/library/urllib.html#urllib.urlopen
-        # http://stackoverflow.com/a/2982
-        tempfile.getcode = types.MethodType( getcode, tempfile )
-        tempfile.geturl = types.MethodType( geturl, tempfile )
-        tempfile.info = types.MethodType( info, tempfile )
+        response = urllib.addinfourl(
+            tempfile,
+            headers,
+            tuf_updater.url,
+            code = 200
+        )
 
-        return tempfile
+        return response
 
     # TODO: replicate complete behaviour of urllib.URLopener.retrieve
     def __tuf_retrieve(
         self,
-        parsed_url,
-        tuf_configuration,
+        tuf_updater,
         filename = None,
         reporthook = None,
         data = None
     ):
         # TODO: set valid headers
-        headers = {}
+        headers = None
         # TUF assumes that target_filepath does not begin with a '/'
-        target_filepath = parsed_url.path.lstrip( '/' )
+        target_filepath = tuf_updater.parsed_url.path.lstrip( '/' )
 
         # if filename does not exist, then we use a temporary directory
         if filename is None:
-            destination_directory, filename = tuf_configuration.make_tempfile(
+            destination_directory, filename = tuf_updater.make_tempfile(
                 target_filepath
             )
         else:
@@ -89,22 +57,23 @@ class TUFancyURLOpener( urllib.FancyURLopener ):
                 destination_directory = filename[ : last_index ]
                 if not os.path.isdir( destination_directory ):
                     destination_directory, filename = \
-                        tuf_configuration.make_tempfile( target_filepath )
+                        tuf_updater.make_tempfile( target_filepath )
             else:
                 destination_directory, filename = \
-                    tuf_configuration.make_tempfile( target_filepath )
+                    tuf_updater.make_tempfile( target_filepath )
 
-        tuf_configuration.switch_context()
-        tuf_configuration.updater.refresh()
+        # TODO: higher-level download abstractions via TUFUpdater
+        tuf_updater.switch_context()
+        tuf_updater.updater.refresh()
 
-        targets = [ tuf_configuration.updater.target( target_filepath ) ]
-        updated_targets = tuf_configuration.updater.updated_targets(
+        targets = [ tuf_updater.updater.target( target_filepath ) ]
+        updated_targets = tuf_updater.updater.updated_targets(
             targets,
             destination_directory
         )
 
         for target in updated_targets:
-            tuf_configuration.updater.download_target(
+            tuf_updater.updater.download_target(
                 target,
                 destination_directory
             )
@@ -112,20 +81,18 @@ class TUFancyURLOpener( urllib.FancyURLopener ):
         return filename, headers
 
     def open( self, fullurl, data = None ):
-        parsed_url = urlparse.urlparse( fullurl )
-        tuf_configuration = _tuf_configurations.get( parsed_url.hostname )
+        tuf_updater = TUFUpdater.make_tuf_updater( fullurl )
 
-        if tuf_configuration is None:
-            return urllib.URLopener.open( self, fullurl, data = data )
+        if tuf_updater is None:
+            return urllib.FancyURLopener.open( self, fullurl, data = data )
         else:
-            return self.__tuf_open( parsed_url, tuf_configuration, data = data )
+            return self.__tuf_open( tuf_updater, data = data )
 
     def retrieve( self, url, filename = None, reporthook = None, data = None ):
-        parsed_url = urlparse.urlparse( url )
-        tuf_configuration = _tuf_configurations.get( parsed_url.hostname )
+        tuf_updater = TUFUpdater.make_tuf_updater( url )
 
-        if tuf_configuration is None:
-            return urllib.URLopener.retrieve(
+        if tuf_updater is None:
+            return urllib.FancyURLopener.retrieve(
                 self,
                 url,
                 filename = filename,
@@ -134,8 +101,7 @@ class TUFancyURLOpener( urllib.FancyURLopener ):
             )
         else:
             return self.__tuf_retrieve(
-                parsed_url,
-                tuf_configuration,
+                tuf_updater,
                 filename = filename,
                 reporthook = reporthook,
                 data = data
@@ -146,12 +112,57 @@ class TUFile( file ):
     pass
 
 
-def map( tuf_configuration ):
-    global _tuf_configurations
+class TUFUpdater( object ):
+    # hostname: str -> tuf_configuration: TUFConfiguration
+    _tuf_configurations = {}
 
-    assert isinstance( tuf_configuration, TUFConfiguration )
-    # TODO: clean up after old configurations
-    _tuf_configurations[ tuf_configuration.hostname ] = tuf_configuration
+    def __init__( self, url, parsed_url, tuf_configuration ):
+        self.url = url
+        self.parsed_url = parsed_url
+        self.tuf_configuration = tuf_configuration
+
+        # must switch context before instantiating updater
+        # because updater depends on some module (tuf.conf) variables
+        self.switch_context()
+        self.updater = tuf.client.updater.Updater(
+            self.parsed_url.hostname,
+            self.tuf_configuration.repository_mirrors
+        )
+
+    @staticmethod
+    def make_tuf_updater( url ):
+        parsed_url = urlparse.urlparse( url )
+        # TODO: enable specificity beyond hostname (e.g. include scheme, port)
+        tuf_configuration = \
+            TUFUpdater._tuf_configurations.get( parsed_url.hostname )
+
+        if tuf_configuration is None:
+            return None
+        else:
+            return TUFUpdater( url, parsed_url, tuf_configuration )
+
+    def make_tempfile( self, target_filepath ):
+        destination_directory = self.tuf_configuration.tempdir
+        filename = os.path.join( destination_directory, target_filepath )
+        return destination_directory, filename
+
+    def switch_context( self ):
+        # Set the local repository directory containing the metadata files.
+        tuf.conf.repository_directory = \
+            self.tuf_configuration.repository_directory
+
+
+# TODO: setup based on JSON file
+def interpose( tuf_configuration ):
+    if isinstance( tuf_configuration, TUFConfiguration ):
+        TUFUpdater._tuf_configurations[
+            tuf_configuration.hostname
+        ] = tuf_configuration
+
+
+def go_away():
+    """Remove TUF interposition and restore previous urllib openers."""
+    raise NotImplementedError
 
 
 # http://docs.python.org/2/library/urllib.html#urllib._urlopener
