5236efdbae87d21a1dacc812b099caa22b511516
dachshund <trishankkarthik@gmail.com>
2013-03-14 17:13:21 -0400

    Revert to previous logger.

--- tuf/interposition/utility.py
+++ tuf/interposition/utility.py
@@ -21,19 +21,16 @@ class Logger(object):
 
   @staticmethod
   def error(message):
-    print message
-    #Logger.__logger.error(message)
-    #Logger.exception(message)
+    Logger.__logger.error(message)
+    Logger.exception(message)
 
 
   @staticmethod
   def exception(message):
-    print message
-    #Logger.__logger.exception(message)
+    Logger.__logger.exception(message)
 
 
   @staticmethod
   def warn(message):
-    print message
-    #Logger.__logger.warn(message)
-    #Logger.exception(message)
+    Logger.__logger.warn(message)
+    Logger.exception(message)
