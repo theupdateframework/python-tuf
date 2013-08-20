49643d63875e8750e63f88acaed58bcd9304ceef
zhengyuyu <nuistzyy@gmail.com>
2013-08-20 03:50:16 -0400

    Add a new kind of slow retrieval attack

--- tuf/tests/system_tests/slow_retrieval_server.py
+++ tuf/tests/system_tests/slow_retrieval_server.py
@@ -25,9 +25,13 @@ import random
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
-DELAY = 1
 
 
+# Modify the HTTPServer class to pass the test_mode argument to do_GET function.
+class HTTPServer_Test(HTTPServer):
+  def __init__(self, server_address, Handler, test_mode):
+    HTTPServer.__init__(self, server_address, Handler)
+    self.test_mode = test_mode
 
 # HTTP request handler.
 class Handler(BaseHTTPRequestHandler):
@@ -43,14 +47,26 @@ class Handler(BaseHTTPRequestHandler):
       self.send_response(200)
       self.send_header('Content-length', str(len(data)))
       self.end_headers()
-
-      # Throttle the file by sending a character every few seconds.
-      for i in range(len(data)):
+      
+      if self.server.test_mode == "mode_1":
+      # before sends any data, the server does nothing during a long time.
+        DELAY = 1000
         time.sleep(DELAY)
-        self.wfile.write(data[i])
+        self.wfile.write(data)
 
-      return
+        return
 
+      else:
+        DELAY = 1
+        send_bytes_per_time = len(data) / 10
+        count = 0
+        # Throttle the file by sending several characters every few seconds.
+        while count <= len(data) / send_bytes_per_time:
+          time.sleep(DELAY)
+          self.wfile.write(data[count * send_bytes_per_time:(count + 1) * send_bytes_per_time])
+          count += 1
+
+        return
     except IOError, e:
       self.send_error(404, 'File Not Found!')
 
@@ -62,18 +78,19 @@ def get_random_port():
 
 
 
-def run(port):
+def run(port, test_mode):
   server_address = ('localhost', port)
-  httpd = HTTPServer(server_address, Handler)
+  httpd = HTTPServer_Test(server_address, Handler, test_mode)
   print('Slow server is active on port: '+str(port)+' ...')
   httpd.handle_request()
 
 
 
 if __name__ == '__main__':
-  if len(sys.argv) > 1:
+  if len(sys.argv) > 2:
     port = int(sys.argv[1])
+    test_mode = sys.argv[2]
   else:
     port = get_random_port()
-
-  run(port)
+    test_mode = None
+  run(port, test_mode)
