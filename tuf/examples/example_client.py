fd39de6665ba8b46dadf553c6f988a4ae32ff172
dachshund <trishankkarthik@gmail.com>
2013-08-27 15:13:55 -0400

    Import chain-of-trust function from @vladdd.

--- tuf/examples/example_client.py
+++ tuf/examples/example_client.py
@@ -1,33 +1,36 @@
 """
 <Program Name>
-  simple_pip_integration.py
+  example_client.py
 
 <Author>
   Vladimir Diaz <vladimir.v.diaz@gmail.com>
 
 <Started>
-  August 1, 2013
+  September 2012
 
 <Copyright>
   See LICENSE for licensing information.
 
 <Purpose>
-  Example client script demonstrating custom python code one can write for a
-  PyPI+pip+TUF integration.
+  Example script demonstrating custom python code a software updater
+  utilizing The Update Framework may write to securely update files.
+  The 'basic_client.py' script can be used on the command-line to perform
+  an update that will download and update all available targets; writing
+  custom code is not required in this case.
   
-  The custom example below demonstrates updating all the targets of a
-  specified role (i.e., 'targets/
+  The custom examples below demonstrate:
+  (1) updating all targets
+  (2) updating all the targets of a specified role
+  (3) updating a specific target explicitely named.
 
 """
 
 import logging
 
-import tuf
-import tuf.log
 import tuf.client.updater
 
-logger = logging.getLogger('tuf.cient.basic_client')
-
+# Uncomment the line below to enable printing of debugging information.
+#tuf.log.set_log_level(logging.DEBUG)
 
 # Set the local repository directory containing the metadata files.
 tuf.conf.repository_directory = '.'
@@ -51,9 +54,6 @@ destination_directory = './targets'
 # all the targets tracked, and determine which of these targets have been
 # updated.
 updater.refresh()
-
-#  
-updater.refresh_targets_metadata_chain(
 all_targets = updater.all_targets()
 updated_targets = updater.updated_targets(all_targets, destination_directory)
 
@@ -67,3 +67,28 @@ for target in updated_targets:
 # Remove any files from the destination directory that are no longer being
 # tracked.
 updater.remove_obsolete_targets(destination_directory)
+
+
+"""
+# Example demonstrating an update that only downloads the targets of
+# a specific role (i.e., 'targets/role1')
+
+updater.refresh()
+targets_of_role1 = updater.targets_of_role('targets/role1')
+updated_targets = updater.updated_targets(targets_of_role1, destination_directory)
+
+for target in updated_targets:
+  updater.download_target(target, destination_directory)
+"""
+
+
+"""
+# Example demonstrating an update that downloads a specific target.
+
+updater.refresh()
+target = updater.target('LICENSE.txt')
+updated_target = updater.updated_targets([target], destination_directory)
+
+for target in updated_target:
+  updater.download_target(target, destination_directory)
+"""
