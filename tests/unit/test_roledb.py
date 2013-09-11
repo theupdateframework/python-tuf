a749c1d6bf59331abcd6faa397c51808a125b78d
vladdd <vladimir.v.diaz@gmail.com>
2013-09-11 13:33:28 -0400

    Fix test_keystore.py unit test failure
    Ensure the keystore is empty prior to running the test cases.  The courtesy bug is spreading.

--- tests/unit/test_roledb.py
+++ tests/unit/test_roledb.py
@@ -404,14 +404,14 @@ class TestRoledb(unittest.TestCase):
 
 
 def setUpModule():
-  # setUpModule is called before any test cases run.
+  # setUpModule() is called before any test cases run.
   # Ensure the roledb has not been modified by a previous test, which may
   # affect assumptions (i.e., empty roledb) made by the tests cases in this
   # unit test.
   tuf.roledb.clear_roledb()
 
 def tearDownModule():
-  # tearDownModule is called after all the tests have run.
+  # tearDownModule() is called after all the tests have run.
   # Ensure we clean up roledb.  Courtesy is contagious, and it begins with
   # test_roledb.py.
   tuf.roledb.clear_roledb()
