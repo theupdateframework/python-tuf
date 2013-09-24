67b886d5e012a190eb55a2cd7dc2aba7008902e0
vladdd <vladimir.v.diaz@gmail.com>
2013-09-23 21:02:17 -0400

    test_endless_data_attack.py: Fix indentation
    Also help with Python 3 compatability.

--- tests/integration/test_endless_data_attack.py
+++ tests/integration/test_endless_data_attack.py
@@ -17,21 +17,26 @@
   Simulate an endless data attack.  A simple client update vs. client
   update implementing TUF.
 
-Note: The interposition provided by 'tuf.interposition' is used to intercept
-all calls made by urllib/urillib2 to certain hostname specified in 
-the interposition configuration file.  Look up interposition.py for more
-information and illustration of a sample contents of the interposition 
-configuration file.  Interposition was meant to make TUF integration with an
-existing software updater an easy process.  This allows for more flexibility
-to the existing software updater.  However, if you are planning to solely use
-TUF there should be no need for interposition, all necessary calls will be
-generated from within TUF.
+  Note: The interposition provided by 'tuf.interposition' is used to intercept
+  all calls made by urllib/urillib2 to certain hostname specified in 
+  the interposition configuration file.  Look up interposition.py for more
+  information and illustration of a sample contents of the interposition 
+  configuration file.  Interposition was meant to make TUF integration with an
+  existing software updater an easy process.  This allows for more flexibility
+  to the existing software updater.  However, if you are planning to solely use
+  TUF there should be no need for interposition, all necessary calls will be
+  generated from within TUF.
 
-Note: There is no difference between 'updates' and 'target' files.
+  There is no difference between 'updates' and 'target' files.
 
 """
 
+# Help with Python 3 compatability, where the print statement is a function, an
+# implicit relative import is invalid, and the '/' operator performs true
+# division.  Example:  print 'hello world' raises a 'SyntaxError' exception.
 from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
 
 import os
 import urllib
