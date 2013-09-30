4dd6d33c34d6fcb069a105be8f033ee1f995669f
vladdd <vladimir.v.diaz@gmail.com>
2013-09-30 10:46:54 -0400

    Update time_ed25519.py following addition of PyNaCl support

--- tuf/time_ed25519.py
+++ tuf/time_ed25519.py
@@ -1,25 +1,32 @@
 from __future__ import print_function, absolute_import, division
+import sys
 import timeit
 
 import tuf
 from tuf.ed25519_key import *
 
-print('Timing ed25519.generate()')
-print(timeit.timeit('generate()',
-                    setup='from __main__ import generate',
+use_pynacl = False
+if '--pynacl' in sys.argv:
+  use_pynacl = True
+  
+print('Timing generate()')
+print(timeit.timeit('generate(use_pynacl)',
+                    setup='from __main__ import generate, use_pynacl',
                     number=1))
 
-print('\nTiming ed25519.create_signature()')
-print(timeit.timeit('create_signature(ed25519_key, data)',
-                    setup='from __main__ import generate, create_signature;\
-                          ed25519_key = generate();\
+print('\nTiming create_signature()')
+print(timeit.timeit('create_signature(ed25519_key, data, use_pynacl)',
+                    setup='from __main__ import generate, create_signature, \
+                                                use_pynacl; \
+                          ed25519_key = generate(use_pynacl);\
                           data = "The quick brown fox jumps over the lazy dog"',
                     number=1))
 
-print('\nTiming ed25519.verify_signature()')
-print(timeit.timeit('verify_signature(ed25519_key, signature, data)',
-                    setup='from __main__ import generate, create_signature, verify_signature;\
-                          ed25519_key = generate();\
+print('\nTiming verify_signature()')
+print(timeit.timeit('verify_signature(ed25519_key, signature, data, use_pynacl)',
+                    setup='from __main__ import generate, create_signature, \
+                                                verify_signature, use_pynacl;\
+                          ed25519_key = generate(use_pynacl);\
                           data = "The quick brown fox jumps over the lazy dog";\
-                          signature = create_signature(ed25519_key, data)',
+                          signature = create_signature(ed25519_key, data, use_pynacl)',
                     number=1))
