f6e0fecf077273d320c0583968f24e508623af3b
Pankh <pg1159@nyu.edu>
2014-10-22 14:44:31 -0400

    Add header to test_interpose_updater

--- tests/test_interpose_updater.py
+++ tests/test_interpose_updater.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
 """
+<Program Name>
+  test_interpose_updater.py
 
+<Author>
+  Pankhuri Goyal <pankhurigoyal02@gmail.com>
+
+<Started>
+  August 2014.
+
+<Copyright>
+  See LICENSE for licensing information.
+
+<Purpose>
+  Unit test for tuf.interposition.updater.
 """
 
 from __future__ import print_function                                           
