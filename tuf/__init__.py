c4ef697ec8e04def7c5ccab6d4ca4372835af932
Sebastien Awwad <sebastien.awwad@nyu.edu>
2016-03-16 16:39:47 -0400

    Fix for newly discovered python 3 issue causing unprintable exceptions

--- tuf/__init__.py
+++ tuf/__init__.py
@@ -332,13 +332,13 @@ class NoWorkingMirrorError(Error):
   def __str__(self):
     all_errors = 'No working mirror was found:'
 
-    for mirror_url, mirror_error in self.mirror_errors.iteritems():
+    for mirror_url, mirror_error in six.iteritems(self.mirror_errors):
       try:
         # http://docs.python.org/2/library/urlparse.html#urlparse.urlparse
         mirror_url_tokens = six.moves.urllib.parse.urlparse(mirror_url)
       
       except:
-        logging.exception('Failed to parse mirror URL: ' + repr(mirror_url))
+        #logging.exception('Failed to parse mirror URL: ' + repr(mirror_url))
         mirror_netloc = mirror_url
       
       else:
