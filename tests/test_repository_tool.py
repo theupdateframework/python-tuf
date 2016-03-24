9c0064e39ddef6e2950ca8d7691053f2fdc292e7
Vladimir Diaz <vladimir.v.diaz@gmail.com>
2016-03-24 13:55:44 -0400

    Fix remaining issues with add_target_to_bin() and remove_target_from_bin()

--- tests/test_repository_tool.py
+++ tests/test_repository_tool.py
@@ -1241,11 +1241,6 @@ class TestTargets(unittest.TestCase):
     
     self.assertRaises(tuf.Error, empty_targets_role.add_target_to_bin,
                       target1_filepath)
-    
-    # Non-bin delegation, although it has a correct hashed bin name.
-    #empty_targets_role.delegate('z', [public_key], [target1_filepath])
-    #self.assertRaises(tuf.Error, empty_targets_role.add_target_to_bin,
-    #                  target1_filepath)
 
     # Test for a required hashed bin that does not exist.
     self.targets_object.revoke('e')
@@ -1275,8 +1270,8 @@ class TestTargets(unittest.TestCase):
     list_of_targets = [target1_filepath] 
 
     # Delegate to hashed bins.  The target filepath to be tested is expected 
-    # to contain a hash prefix of 'e', so it should be added to the
-    # 'targets/e' role.
+    # to contain a hash prefix of 'e', and can be accessed as:
+    # repository.targets('e').
     self.targets_object.delegate_hashed_bins(list_of_targets, public_keys,
                                              number_of_bins=16)
  
@@ -1289,7 +1284,7 @@ class TestTargets(unittest.TestCase):
     self.targets_object.add_target_to_bin(target1_filepath)
     
     for delegation in self.targets_object.delegations:
-      if delegation.rolename == 'targets/e':
+      if delegation.rolename == 'e':
         self.assertTrue('/file1.txt' in delegation.target_files)
       
       else:
@@ -1300,7 +1295,7 @@ class TestTargets(unittest.TestCase):
     self.targets_object.remove_target_from_bin(target1_filepath)
     
     for delegation in self.targets_object.delegations:
-      if delegation.rolename == 'targets/e':
+      if delegation.rolename == 'e':
         self.assertTrue('/file1.txt' not in delegation.target_files)
       
       else:
