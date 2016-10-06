c00d66e24685a31db8dc2e3c41005de884103839
Trishank Karthik Kuppusamy <trishank@nyu.edu>
2016-10-06 14:33:44 -0400

    Add encoder for bootloader module messages.

--- compile-application-module.sh
+++ compile-application-module.sh
@@ -2,6 +2,6 @@
 
 ./clean.sh
 # TODO: May want to consider reorganizing how type definitions are shared.
-asn1c -pdu=auto CommonModule.asn1 MetadataModule.asn1  RootModule.asn1 TargetsModule.asn1 SnapshotModule.asn1 TimestampModule.asn1 BootloaderModule.asn1 ApplicationModule.asn1
+asn1c -pdu=auto CommonModule.asn1 MetadataModule.asn1  RootModule.asn1 TargetsModule.asn1 SnapshotModule.asn1 TimestampModule.asn1 TimeServerModule.asn1 BootloaderModule.asn1 ApplicationModule.asn1
 mv Makefile.am.sample Makefile
 make
