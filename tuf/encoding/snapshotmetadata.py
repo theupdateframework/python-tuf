7ba958418e95d48a42d94804764564ae5aef0592
Sebastien Awwad <sebastien.awwad@gmail.com>
2017-02-07 17:34:14 -0500

    fixing asn1 imports to work within TUF

--- tuf/encoding/snapshotmetadata.py
+++ tuf/encoding/snapshotmetadata.py
@@ -7,9 +7,9 @@
 
 from pyasn1.type import univ, char, namedtype, namedval, tag, constraint, useful
 
-from metadataverificationmodule import *
+from uptane.encoding.metadataverificationmodule import *
 
-import metadata
+import uptane.encoding.metadata as metadata
 
 
 def get_asn_signed(json_signed):
