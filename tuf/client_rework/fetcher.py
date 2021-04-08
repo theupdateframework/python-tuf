3f89c018b3dac0dd71d7f8dae8834df66bfde49d
Teodora Sechkova <tsechkova@vmware.com>
2021-04-08 12:43:37 +0300

    Reformat client code
    Run black and isort over the old modules which were
    moved inside the client directory:
    - download.py
    - fetcher.py
    - mirrors.py
    - requests_fetcher.py
    
    Signed-off-by: Teodora Sechkova <tsechkova@vmware.com>

--- tuf/client_rework/fetcher.py
+++ tuf/client_rework/fetcher.py
@@ -7,32 +7,33 @@
 # Imports
 import abc
 
+
 # Classes
-class FetcherInterface():
-  """Defines an interface for abstract network download.
+class FetcherInterface:
+    """Defines an interface for abstract network download.
 
-  By providing a concrete implementation of the abstract interface,
-  users of the framework can plug-in their preferred/customized
-  network stack.
-  """
-
-  __metaclass__ = abc.ABCMeta
-
-  @abc.abstractmethod
-  def fetch(self, url, required_length):
-    """Fetches the contents of HTTP/HTTPS url from a remote server.
-
-    Ensures the length of the downloaded data is up to 'required_length'.
-
-    Arguments:
-      url: A URL string that represents a file location.
-      required_length: An integer value representing the file length in bytes.
-
-    Raises:
-      tuf.exceptions.SlowRetrievalError: A timeout occurs while receiving data.
-      tuf.exceptions.FetcherHTTPError: An HTTP error code is received.
-
-    Returns:
-      A bytes iterator
+    By providing a concrete implementation of the abstract interface,
+    users of the framework can plug-in their preferred/customized
+    network stack.
     """
-    raise NotImplementedError # pragma: no cover
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def fetch(self, url, required_length):
+        """Fetches the contents of HTTP/HTTPS url from a remote server.
+
+        Ensures the length of the downloaded data is up to 'required_length'.
+
+        Arguments:
+          url: A URL string that represents a file location.
+          required_length: An integer value representing the file length in bytes.
+
+        Raises:
+          tuf.exceptions.SlowRetrievalError: A timeout occurs while receiving data.
+          tuf.exceptions.FetcherHTTPError: An HTTP error code is received.
+
+        Returns:
+          A bytes iterator
+        """
+        raise NotImplementedError  # pragma: no cover
