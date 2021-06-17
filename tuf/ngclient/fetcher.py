1596de24545637d0912f1943fb87e66c5b340a4e
Teodora Sechkova <tsechkova@vmware.com>
2021-06-17 15:37:50 +0300

    Move download.py functions to FetcherInterface
    Make download_file and download_bytes functions part of
    the FetcherInterface class. Remove download.py module.
    
    Signed-off-by: Teodora Sechkova <tsechkova@vmware.com>

--- tuf/ngclient/fetcher.py
+++ tuf/ngclient/fetcher.py
@@ -6,7 +6,15 @@
 
 # Imports
 import abc
-from typing import Iterator
+import logging
+import tempfile
+from contextlib import contextmanager
+from typing import IO, Iterator
+from urllib import parse
+
+from tuf import exceptions
+
+logger = logging.getLogger(__name__)
 
 
 # Classes
@@ -40,3 +48,51 @@ class FetcherInterface:
             A bytes iterator
         """
         raise NotImplementedError  # pragma: no cover
+
+    @contextmanager
+    def download_file(self, url: str, required_length: int) -> Iterator[IO]:
+        """Opens a connection to 'url' and downloads the content
+        up to 'required_length'.
+
+        Args:
+          url: a URL string that represents the location of the file.
+          required_length: an integer value representing the length of
+              the file or an upper boundary.
+
+        Raises:
+          DownloadLengthMismatchError: a mismatch of observed vs expected
+              lengths while downloading the file.
+
+        Yields:
+          A file object that points to the contents of 'url'.
+        """
+        # 'url.replace('\\', '/')' is needed for compatibility with
+        # Windows-based systems, because they might use back-slashes in place
+        # of forward-slashes. This converts it to the common format.
+        # unquote() replaces %xx escapes in a url with their single-character
+        # equivalent. A back-slash may beencoded as %5c in the url, which
+        # should also be replaced with a forward slash.
+        url = parse.unquote(url).replace("\\", "/")
+        logger.debug("Downloading: %s", url)
+
+        number_of_bytes_received = 0
+
+        with tempfile.TemporaryFile() as temp_file:
+            chunks = self.fetch(url, required_length)
+            for chunk in chunks:
+                temp_file.write(chunk)
+                number_of_bytes_received += len(chunk)
+            if number_of_bytes_received > required_length:
+                raise exceptions.DownloadLengthMismatchError(
+                    required_length, number_of_bytes_received
+                )
+            temp_file.seek(0)
+            yield temp_file
+
+    def download_bytes(self, url: str, required_length: int) -> bytes:
+        """Download bytes from given url
+
+        Returns the downloaded bytes, otherwise like download_file()
+        """
+        with self.download_file(url, required_length) as dl_file:
+            return dl_file.read()
