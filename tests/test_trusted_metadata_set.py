726af73256be9c509c4aa1662c11f72abde7c1be
Martin Vrachev <mvrachev@vmware.com>
2021-06-29 19:14:56 +0300

    ng client metadata set: organize common test code
    Move the shared code between tests into the "setupClass" function.
    
    Signed-off-by: Martin Vrachev <mvrachev@vmware.com>

--- tests/test_trusted_metadata_set.py
+++ tests/test_trusted_metadata_set.py
@@ -16,36 +16,37 @@ logger = logging.getLogger(__name__)
 
 class TestTrustedMetadataSet(unittest.TestCase):
 
-    def test_update(self):
-        repo_dir = os.path.join(os.getcwd(), 'repository_data', 'repository', 'metadata')
+    @classmethod
+    def setUpClass(cls):
+        cls.repo_dir = os.path.join(
+            os.getcwd(), 'repository_data', 'repository', 'metadata'
+        )
+        cls.metadata = {}
+        for md in ["root", "timestamp", "snapshot", "targets", "role1", "role2"]:
+            with open(os.path.join(cls.repo_dir, f"{md}.json"), "rb") as f:
+                cls.metadata[md] = f.read()
 
-        with open(os.path.join(repo_dir, "root.json"), "rb") as f:
-            trusted_set = TrustedMetadataSet(f.read())
+
+    def test_update(self):
+        trusted_set = TrustedMetadataSet(self.metadata["root"])
         trusted_set.root_update_finished()
 
-        with open(os.path.join(repo_dir, "timestamp.json"), "rb") as f:
-            trusted_set.update_timestamp(f.read())
-        with open(os.path.join(repo_dir, "snapshot.json"), "rb") as f:
-            trusted_set.update_snapshot(f.read())
-        with open(os.path.join(repo_dir, "targets.json"), "rb") as f:
-            trusted_set.update_targets(f.read())
-        with open(os.path.join(repo_dir, "role1.json"), "rb") as f:
-            trusted_set.update_delegated_targets(f.read(), "role1", "targets")
-        with open(os.path.join(repo_dir, "role2.json"), "rb") as f:
-            trusted_set.update_delegated_targets(f.read(), "role2", "role1")
+        trusted_set.update_timestamp(self.metadata["timestamp"])
+        trusted_set.update_snapshot(self.metadata["snapshot"])
+        trusted_set.update_targets(self.metadata["targets"])
+        trusted_set.update_delegated_targets(
+            self.metadata["role1"], "role1", "targets"
+        )
+        trusted_set.update_delegated_targets(
+            self.metadata["role2"], "role2", "role1"
+        )
 
     def test_out_of_order_ops(self):
-        repo_dir = os.path.join(os.getcwd(), 'repository_data', 'repository', 'metadata')
-        data={}
-        for md in ["root", "timestamp", "snapshot", "targets", "role1"]:
-            with open(os.path.join(repo_dir, f"{md}.json"), "rb") as f:
-                data[md] = f.read()
-
-        trusted_set = TrustedMetadataSet(data["root"])
+        trusted_set = TrustedMetadataSet(self.metadata["root"])
 
         # Update timestamp before root is finished
         with self.assertRaises(RuntimeError):
-            trusted_set.update_timestamp(data["timestamp"])
+            trusted_set.update_timestamp(self.metadata["timestamp"])
 
         trusted_set.root_update_finished()
         with self.assertRaises(RuntimeError):
@@ -53,50 +54,53 @@ class TestTrustedMetadataSet(unittest.TestCase):
 
         # Update snapshot before timestamp
         with self.assertRaises(RuntimeError):
-            trusted_set.update_snapshot(data["snapshot"])
+            trusted_set.update_snapshot(self.metadata["snapshot"])
 
-        trusted_set.update_timestamp(data["timestamp"])
+        trusted_set.update_timestamp(self.metadata["timestamp"])
 
         # Update targets before snapshot
         with self.assertRaises(RuntimeError):
-            trusted_set.update_targets(data["targets"])
+            trusted_set.update_targets(self.metadata["targets"])
 
-        trusted_set.update_snapshot(data["snapshot"])
+        trusted_set.update_snapshot(self.metadata["snapshot"])
 
-        #update timestamp after snapshot
+        # update timestamp after snapshot
         with self.assertRaises(RuntimeError):
-            trusted_set.update_timestamp(data["timestamp"])
+            trusted_set.update_timestamp(self.metadata["timestamp"])
 
         # Update delegated targets before targets
         with self.assertRaises(RuntimeError):
-            trusted_set.update_delegated_targets(data["role1"], "role1", "targets")
+            trusted_set.update_delegated_targets(
+                self.metadata["role1"], "role1", "targets"
+            )
 
-        trusted_set.update_targets(data["targets"])
-        trusted_set.update_delegated_targets(data["role1"], "role1", "targets")
+        trusted_set.update_targets(self.metadata["targets"])
+        trusted_set.update_delegated_targets(
+            self.metadata["role1"], "role1", "targets"
+        )
+
+        trusted_set.update_targets(self.metadata["targets"])
+        trusted_set.update_delegated_targets(
+            self.metadata["role1"], "role1", "targets"
+        )
 
     def test_update_with_invalid_json(self):
-        repo_dir = os.path.join(os.getcwd(), 'repository_data', 'repository', 'metadata')
-        data={}
-        for md in ["root", "timestamp", "snapshot", "targets", "role1"]:
-            with open(os.path.join(repo_dir, f"{md}.json"), "rb") as f:
-                data[md] = f.read()
-
         # root.json not a json file at all
         with self.assertRaises(exceptions.RepositoryError):
             TrustedMetadataSet(b"")
         # root.json is invalid
-        root = Metadata.from_bytes(data["root"])
+        root = Metadata.from_bytes(self.metadata["root"])
         root.signed.version += 1
         with self.assertRaises(exceptions.RepositoryError):
             TrustedMetadataSet(json.dumps(root.to_dict()).encode())
 
-        trusted_set = TrustedMetadataSet(data["root"])
+        trusted_set = TrustedMetadataSet(self.metadata["root"])
         trusted_set.root_update_finished()
 
         top_level_md = [
-            (data["timestamp"], trusted_set.update_timestamp),
-            (data["snapshot"], trusted_set.update_snapshot),
-            (data["targets"], trusted_set.update_targets),
+            (self.metadata["timestamp"], trusted_set.update_timestamp),
+            (self.metadata["snapshot"], trusted_set.update_snapshot),
+            (self.metadata["targets"], trusted_set.update_targets),
         ]
         for metadata, update_func in top_level_md:
             # metadata is not json
@@ -110,7 +114,7 @@ class TestTrustedMetadataSet(unittest.TestCase):
 
             # metadata is of wrong type
             with self.assertRaises(exceptions.RepositoryError):
-                update_func(data["root"])
+                update_func(self.metadata["root"])
 
             update_func(metadata)
 
