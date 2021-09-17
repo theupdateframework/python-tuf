7b61ad853885086aba756ccbb467d490247323d5
Teodora Sechkova <tsechkova@vmware.com>
2021-09-17 18:08:32 +0300

    ngclient: use mock instead of slow_retrieval_server
    Instead of starting a dedicated slow_retrieval_server
    to test for read timeout in RequestsFetcher, use
    unittest.mock to mock the response.raw.read call.
    
    Signed-off-by: Teodora Sechkova <tsechkova@vmware.com>

--- tests/test_fetcher_ng.py
+++ tests/test_fetcher_ng.py
@@ -19,7 +19,7 @@ import requests
 from tests import utils
 from tuf import exceptions, unittest_toolbox
 from tuf.ngclient._internal.requests_fetcher import RequestsFetcher
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 logger = logging.getLogger(__name__)
 
@@ -110,19 +110,17 @@ class TestFetcher(unittest_toolbox.Modified_TestCase):
             self.fetcher.fetch(self.url)
         self.assertEqual(cm.exception.status_code, 404)
 
-    # Read timeout error
-    def test_read_timeout(self):
-        # Reduce the read socket timeout to speed up the test
-        # while keeping the connect timeout
-        default_socket_timeout = self.fetcher.socket_timeout
-        self.fetcher.socket_timeout = (default_socket_timeout, 0.1)
-        # Launch a new "slow retrieval" server sending one byte each 40s
-        slow_server_process_handler = utils.TestServerProcess(log=logger, server='slow_retrieval_server.py')
-        self.url = f"http://{utils.TEST_HOST_ADDRESS}:{str(slow_server_process_handler.port)}/{self.rel_target_filepath}"
+    # Response read timeout error
+    @patch.object(requests.Session, 'get')
+    def test_response_read_timeout(self, mock_session_get):
+        mock_response = Mock()
+        attr = {'raw.read.side_effect': urllib3.exceptions.ReadTimeoutError(None, None, "Read timed out.")}
+        mock_response.configure_mock(**attr)
+        mock_session_get.return_value = mock_response
+
         with self.assertRaises(exceptions.SlowRetrievalError):
             next(self.fetcher.fetch(self.url))
-
-        slow_server_process_handler.clean()
+        mock_response.raw.read.assert_called_once()
 
     # Read/connect session timeout error
     @patch.object(requests.Session, 'get', side_effect=urllib3.exceptions.TimeoutError)
