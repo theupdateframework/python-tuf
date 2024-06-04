873c632da0fd9114ec228ccfaf49eafaab86ec9e
Kapil Sharma <ks3913688@gmail.com>
2024-06-04 12:02:27 +0530

    removing @unittest.expectedFailure tag
    Signed-off-by: Kapil Sharma <ks3913688@gmail.com>

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -186,7 +186,6 @@ class TestRepository(unittest.TestCase):
         self.assertEqual(2, len(snapshot_versions))
         self.assertEqual(2, snapshot_versions[-1].signed.version)
 
-    @unittest.expectedFailure  # Issue 2438
     def test_do_snapshot_after_snapshot_key_change(self) -> None:
         # change snapshot signing keys
         with self.repo.edit_root() as root:
@@ -228,9 +227,7 @@ class TestRepository(unittest.TestCase):
         self.assertEqual(2, len(timestamp_versions))
         self.assertEqual(2, timestamp_versions[-1].signed.version)
 
-    @unittest.expectedFailure  # Issue 2438
-    def test_do_timestamp_after_timestamp_key_change(self) -> None:
-        # change timestamp signing keys
+    def test_do_timestamp_after_timestamp_key_change(self) -> None:        # change timestamp signing keys
         with self.repo.edit_root() as root:
             # remove key
             keyid = root.roles["timestamp"].keyids[0]
