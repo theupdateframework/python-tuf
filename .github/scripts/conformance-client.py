ce560215bf141c81083dc2f66902614141802705
Jussi Kukkonen <jkukkonen@google.com>
2024-08-08 15:48:13 +0300

    Update tuf-conformance action to 1.0
    Also update the client-under-test script
    (this is a direct copy from tuf-conformance).
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

--- .github/scripts/conformance-client.py
+++ .github/scripts/conformance-client.py
@@ -8,9 +8,8 @@ import argparse
 import os
 import shutil
 import sys
-from datetime import datetime, timedelta, timezone
 
-from tuf.ngclient import Updater, UpdaterConfig
+from tuf.ngclient import Updater
 
 
 def init(metadata_dir: str, trusted_root: str) -> None:
@@ -21,23 +20,13 @@ def init(metadata_dir: str, trusted_root: str) -> None:
     print(f"python-tuf test client: Initialized repository in {metadata_dir}")
 
 
-def refresh(
-    metadata_url: str,
-    metadata_dir: str,
-    days_in_future: str,
-    max_root_rotations: int,
-) -> None:
+def refresh(metadata_url: str, metadata_dir: str) -> None:
     """Refresh local metadata from remote"""
 
     updater = Updater(
         metadata_dir,
         metadata_url,
-        config=UpdaterConfig(max_root_rotations=int(max_root_rotations)),
     )
-    if days_in_future != "0":
-        day_int = int(days_in_future)
-        day_in_future = datetime.now(timezone.utc) + timedelta(days=day_int)
-        updater._trusted_set.reference_time = day_in_future  # noqa: SLF001
     updater.refresh()
     print(f"python-tuf test client: Refreshed metadata in {metadata_dir}")
 
@@ -56,7 +45,6 @@ def download_target(
         metadata_url,
         download_dir,
         target_base_url,
-        config=UpdaterConfig(prefix_targets_with_hash=False),
     )
     target_info = updater.get_targetinfo(target_name)
     if not target_info:
@@ -73,10 +61,6 @@ def main() -> int:
     parser.add_argument("--target-name", required=False)
     parser.add_argument("--target-dir", required=False)
     parser.add_argument("--target-base-url", required=False)
-    parser.add_argument("--days-in-future", required=False, default="0")
-    parser.add_argument(
-        "--max-root-rotations", required=False, default=32, type=int
-    )
 
     sub_command = parser.add_subparsers(dest="sub_command")
     init_parser = sub_command.add_parser(
@@ -104,8 +88,6 @@ def main() -> int:
         refresh(
             command_args.metadata_url,
             command_args.metadata_dir,
-            command_args.days_in_future,
-            command_args.max_root_rotations,
         )
     elif command_args.sub_command == "download":
         download_target(
