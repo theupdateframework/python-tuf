6d666968df10064a245fe699ccb3da89e02d9359
Jussi Kukkonen <jkukkonen@google.com>
2025-08-22 18:42:50 +0300

    tests: Expand parallel refresh test
    Use get_targetinfo() so that the delegated role loading is tested as
    well
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

--- tests/refresh_script.py
+++ tests/refresh_script.py
@@ -2,7 +2,7 @@ import sys
 
 from tuf.ngclient import Updater
 
-print(f"Creating and refreshing a client {sys.argv[1]} times:")
+print(f"Fetching metadata {sys.argv[1]} times:")
 print(f"  metadata dir: {sys.argv[2]}")
 print(f"  metadata url: {sys.argv[3]}")
 
@@ -10,6 +10,7 @@ print(f"  metadata url: {sys.argv[3]}")
 for i in range(int(sys.argv[1])):
     try:
         u = Updater(metadata_dir=sys.argv[2], metadata_base_url=sys.argv[3])
-        u.refresh()
+        # file3.txt is delegated so we end up exercising all metadata load paths
+        u.get_targetinfo("file3.txt")
     except OSError as e:
         sys.exit(f"Failed on iteration {i}: {e}")
